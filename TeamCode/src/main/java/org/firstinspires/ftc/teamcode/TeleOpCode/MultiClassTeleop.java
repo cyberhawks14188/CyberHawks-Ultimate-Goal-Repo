f260847605b79e770511d868ba4ecf323aba7c89
jackj <jackjaros06@gmail.com>
2021-03-28 15:22:51 -0500

    commented all the TeleOp subsystems

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpCode/MultiClassTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpCode/MultiClassTeleop.java
@@ -152,7 +152,7 @@ public class MultiClassTeleop extends LinearOpMode {
                 //runs the calculations for TeleOp if we are not in the powershot subsytem to let us run a normal TeleOp
                 DrivetrainClass.DriveBase(gamepad1.left_stick_x, gamepad1.left_stick_y, gamepad1.right_stick_x, gamepad1.right_bumper);
                 RingClass.RingSystemControl(gamepad1.dpad_down, gamepad1.dpad_up, gamepad1.dpad_right, gamepad1.dpad_left, gamepad1.a, gamepad1.b, Ring1Color.red, Ring2Color.red, Ring3Color.red, gamepad1.back, ShooterClass.sotAngleSetReturn());
-                ShooterClass.shooterControl(gamepad1.left_bumper, robot.SOT_M.getCurrentPosition(), getRuntime(), robot.SOT_PT.getVoltage(), RingClass.intakePowerReturn(), gamepad2.right_stick_y);
+                ShooterClass.shooterControl(gamepad1.left_bumper, robot.SOT_M.getCurrentPosition(), getRuntime(), robot.SOT_PT.getVoltage(), RingClass.intakePowerReturn());
                 WobbleArmClass.WobbleControl(gamepad1.left_trigger, robot.WB_PT.getVoltage());
                 //sets motor power if we are not in the powershot subsystem because we control the motor power differently
                 robot.LF_M.setPower(DrivetrainClass.LFMReturn());
